81f9230715927f89aa02c8695fa8052f8110df22
Murtaza Raza <murtazaraza2286@gmail.com>
2026-02-17 22:20:22 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 
 import React, { useState, useRef } from 'react';
-import Navbar from './components/Navbar';
-import Button from './components/Button';
-import { geminiService } from './services/geminiService';
+import Navbar from './Navbar';
+import Button from './Button';
+import { geminiService } from './geminiService';
 import { GenerationResult, GenerationStatus } from './types';
 
 const App: React.FC = () => {
